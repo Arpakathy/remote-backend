60c56dd77e1c9e4094e03f29de7d86f050ae7f3a
Aarpakathy <kathyghomo@gmail.com>
2024-02-05 14:11:53 +0100

    fifth commit

--- remote_backend/remote_backend.tf
+++ remote_backend/remote_backend.tf
@@ -1,4 +1,19 @@
 terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.67.0" 
+    }
+  }
+}
+
+# configure the AWS provider
+
+provider "aws" {
+  region =  "us-east-2"
+}
+
+terraform {
   backend "s3" {
     bucket = aws_s3_bucket.buck-1.id
     key    = "terraform.tfstate"
